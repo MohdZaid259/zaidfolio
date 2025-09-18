dfe46fa82dcdbb77cba75bcc0e64922fbcddc78b
Mohd Zaid <razvizaid259@gmail.com>
2025-09-18 13:24:01 +0530

    temporarly stars fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,7 +95,6 @@ export default function RootLayout({
         )}
       >
         <SplashScreen />
-        {/* <SmoothCursor /> */}
         <StarsCanvas />
         <Navbar />
         {children}
--- src/components/main/star-background.tsx
+++ src/components/main/star-background.tsx
@@ -1,32 +1,53 @@
 "use client";
-import { MotionSpan } from "@/lib/motionElements";
 
-export const StarsCanvas = () => {
-  const stars = Array.from({ length: 100 }, (_, i) => i);
+import { Points, PointMaterial } from "@react-three/drei";
+import { Canvas, type PointsProps, useFrame } from "@react-three/fiber";
+import * as random from "maath/random";
+import { useState, useRef, Suspense } from "react";
+import type { Points as PointsType } from "three";
+
+export const StarBackground = (props: PointsProps) => {
+  const ref = useRef<PointsType | null>(null);
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
+
+  useFrame((_state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 30;
+      ref.current.rotation.y -= delta / 45;
+    }
+  });
 
   return (
-    <div className="fixed inset-0 -z-10 bg-black overflow-hidden">
-      {stars.map((i) => (
-        <MotionSpan
-          key={i}
-          className="absolute w-[2px] h-[2px] rounded-full bg-white"
-          style={{
-            top: `${Math.random() * 100}%`,
-            left: `${Math.random() * 100}%`,
-          }}
-          animate={{
-            y: [0, Math.random() * 200 - 100],
-            x: [0, Math.random() * 200 - 100],
-            opacity: [0.2, 1, 0.2],
-          }}
-          transition={{
-            duration: Math.random() * 40 + 20,
-            repeat: Infinity,
-            repeatType: "mirror",
-            ease: "easeInOut",
-          }}
+    // @ts-ignore
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        stride={3}
+        positions={new Float32Array(sphere)}
+        frustumCulled
+        {...props}
+      >
+        <PointMaterial
+          transparent
+          color="#fff"
+          size={0.002}
+          sizeAttenuation
+          depthWrite={false}
         />
-      ))}
-    </div>
+      </Points>
+    {/* @ts-expect-error */}
+    </group>
   );
-}
+};
+
+export const StarsCanvas = () => (
+  <div className="w-full h-auto fixed inset-0 -z-10">
+    <Canvas camera={{ position: [0, 0, 1] }}>
+      <Suspense fallback={null}>
+        <StarBackground />
+      </Suspense>
+    </Canvas>
+  </div>
+);
